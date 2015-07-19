f90b6144ea9dec8628f67674864a7280839dce99
Mikhail Kever <mikhail.kever@gmail.com>
2015-07-19 11:05:51 -0700

    Emails

--- spemail.py
+++ spemail.py
@@ -4,49 +4,23 @@ import requests
 def generateContent():
     res = {}
     res["from"] = { "name": "BattleBrains" , "email": "sandbox@sparkpostbox.com"}
-    res["subject"] = "Your CodeReview puzzle"
-    res["text"] = """
-tatic int asn1_cb(const char *elem, int len, 
-  void *bitstr)
-{
-  ....
-  case ASN1_GEN_FLAG_FORMAT:
-  if (!strncmp(vstart, "ASCII", 5))
-    arg->format = ASN1_GEN_FORMAT_ASCII;
-  else if (!strncmp(vstart, "UTF8", 4))
-    arg->format = ASN1_GEN_FORMAT_UTF8;
-  else if (!strncmp(vstart, "HEX", 3))
-    arg->format = ASN1_GEN_FORMAT_HEX;
-  else if (!strncmp(vstart, "BITLIST", 3))
-    arg->format = ASN1_GEN_FORMAT_BITLIST;
-  else
-  ....
-}
-"""
+    res["subject"] = "BattleBrains game: your move"
+    res["text"] = """Your puzzle is here: http://bb2ah8.ddns.net/images/{{puzzle}} """
     res["html"] = """
-<body>
-<p>
-<pre>
-static int asn1_cb(const char *elem, int len, 
-  void *bitstr)
-{
-  ....
-  case ASN1_GEN_FLAG_FORMAT:
-  if (!strncmp(vstart, "ASCII", 5))
-    arg->format = ASN1_GEN_FORMAT_ASCII;
-  else if (!strncmp(vstart, "UTF8", 4))
-    arg->format = ASN1_GEN_FORMAT_UTF8;
-  else if (!strncmp(vstart, "HEX", 3))
-    arg->format = ASN1_GEN_FORMAT_HEX;
-  else if (!strncmp(vstart, "BITLIST", 3))
-    arg->format = ASN1_GEN_FORMAT_BITLIST;
-  else
-  ....
-}
-</pre>
-</p>
-</body>
-"""
+        <body>
+        <p> <img src = 'http://bb2ah8.ddns.net/images/{{puzzle}}'> </p>
+        </body>
+    """
+    return res
+
+def generateDailyHighlightsContent():
+    res = {}
+    res["from"] = { "name": "BattleBrains" , "email": "sandbox@sparkpostbox.com"}
+    res["subject"] = "BattleBrains daily highlights"
+    text = open('text.html', 'r').read()
+    res["text"] = text
+    res["html"] = text
+
     return res
 
 def generateRecipients():
@@ -54,19 +28,35 @@ def generateRecipients():
     recipients.append({"address": {"email": "skidanovalexander@gmail.com", "name": "SkidanovAlex"}})
     return recipients
 
+imageId = 0
+totalImages = 2
+
 def generateEmail():
+    global imageId
     res = {}
-    res["substitution_data"] = {}
+    imageId = (imageId + 1) % totalImages + 1
+    res["substitution_data"] = { 'puzzle': 'puzzle%s.png'%imageId }
     res["content"] = generateContent()
     res["recipients"] = generateRecipients()
     return res
 
-def sendEmail():
-    email = generateEmail()
+def generateDailyHighlightsEmail():
+    res = {}
+    res["substitution_data"] = {}
+    res["content"] = generateDailyHighlightsContent()
+    res["recipients"] = generateRecipients()
+    return res
 
+def sendIt(email):
     url = "https://api.sparkpost.com/api/v1/transmissions"
     print json.dumps(email)
     headers = {'Content-Type': 'application/json', 'Authorization': 'b8ad75e62a7b0d8da96155f7944827daff9711ff'}
     r = requests.post(url, data=json.dumps(email), headers=headers)
     print r.text
     r.raise_for_status()
+
+def sendEmail():
+    sendIt(generateEmail())
+
+def sendDailyHighlightsEmail():
+    sendIt(generateDailyHighlightsEmail())
