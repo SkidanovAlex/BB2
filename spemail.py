96bd731fd7bbaf5bc0e3b98d5ed0ba5875ee7c41
Mikhail Kever <mikhail.kever@gmail.com>
2015-07-19 11:57:52 -0700

    email puzzles

--- spemail.py
+++ spemail.py
@@ -5,10 +5,13 @@ def generateContent():
     res = {}
     res["from"] = { "name": "BattleBrains" , "email": "sandbox@sparkpostbox.com"}
     res["subject"] = "BattleBrains game: your move"
-    res["text"] = """Your puzzle is here: http://bb2ah8.ddns.net/images/{{puzzle}} """
+    res["text"] = """Your puzzle is here: http://bb2ah8.ddns.net/images/{{puzzle}} 
+        {{{puzzleDescription}}}
+    """
     res["html"] = """
         <body>
         <p> <img src = 'http://bb2ah8.ddns.net/images/{{puzzle}}'> </p>
+        <p> {{{puzzleDescription}}} </p>
         </body>
     """
     return res
@@ -34,10 +37,15 @@ totalImages = 2
 def generateEmail():
     global imageId
     res = {}
-    imageId = (imageId + 1) % totalImages + 1
-    res["substitution_data"] = { 'puzzle': 'puzzles%s.png'%imageId }
+    descriptions = [
+            "For the first puzzle give the answer with an absolute error of at most 0.001.<br/> For the second puzzle give the length of the path.", 
+            "For the first puzzle give the number of the line with a bug.<br/> For the second puzzle give the first move in any popular chess notation."
+            ]
+    res["substitution_data"] = { 'puzzle': 'puzzles%s.png'%(imageId+1), 'puzzleDescription': descriptions[imageId] }
     res["content"] = generateContent()
     res["recipients"] = generateRecipients()
+    imageId = imageId + 1
+    imageId = imageId % totalImages
     return res
 
 def generateDailyHighlightsEmail():
